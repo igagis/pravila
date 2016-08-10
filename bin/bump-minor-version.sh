e45ce96ad6f337f8fd53ce0244f6d676bdf1b70d
Ivan Gagis <igagis@gmail.com>
2016-08-10 17:33:01 +0300

    bump nimor version script added

---
+++ bin/bump-minor-version.sh
@@ -0,0 +1,34 @@
+#Script for quick bumping up the minor version,
+#i.e. version maj.min+1.0.
+#Minor version from debian changelog will be incremented
+#and a comment passed as argument is added.
+
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+    echo -e "Error: comment as argument expected"
+    exit 1
+fi
+
+
+version=$(prorab-deb-version.sh debian/changelog)
+
+#echo $version
+
+maj=$(echo $version | sed -n -e 's/^\([0-9]*\)\.[0-9]*\.[0-9]*$/\1/p')
+min=$(echo $version | sed -n -e 's/^[0-9]*\.\([0-9]*\)\.[0-9]*$/\1/p')
+rev=$(echo $version | sed -n -e 's/^[0-9]*\.[0-9]*\.\([0-9]*\)$/\1/p')
+
+echo maj = $maj
+echo min = $min
+echo rev = $rev
+
+newver=$maj.$((min+1)).0
+
+echo newver = $newver
+
+if [[ -z ${DEBEMAIL+x} ]]; then echo "error: DEBEMAIL is unset"; exit 1; fi
+if [[ -z ${DEBFULLNAME+x} ]]; then echo "error: DEBFULLNAME is unset"; exit 1; fi
+
+dch -v"$newver" "$1"
+if [[ $? -ne 0 ]]; then exit 1; fi
